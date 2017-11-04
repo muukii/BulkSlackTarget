d2390399637d0dbb62544efb8e5a8ff2ef4dd6f1
Muukii <m@muukii.me>
2017-11-05 04:25:11 +0900

    Initial Commit

---
+++ BulkSlackTarget/BulkSlackTarget.h
@@ -0,0 +1,19 @@
+//
+//  BulkSlackTarget.h
+//  BulkSlackTarget
+//
+//  Created by muukii on 11/5/17.
+//  Copyright © 2017 muukii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BulkSlackTarget.
+FOUNDATION_EXPORT double BulkSlackTargetVersionNumber;
+
+//! Project version string for BulkSlackTarget.
+FOUNDATION_EXPORT const unsigned char BulkSlackTargetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BulkSlackTarget/PublicHeader.h>
+
+
